09d0b38f28d9148e5f5c5ab29612c78d7a452bd8
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 09:44:30 +0500

    Add files via upload

---
+++ task34.js
@@ -0,0 +1,9 @@
+"use strict";
+// task 34 make array of 3 of your favourite pizza
+let pizzas = ["Afghani ", "Pepperoni", "Fajita"];
+console.log("\npizza names:\n\n");
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+    console.log("I like " + pizzas[i] + " pizza");
+}
+console.log("\nI really love pizza!\n\n");
